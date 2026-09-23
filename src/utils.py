8108379c952e160c10defefaf9a70b7cb4f1b29b
SaiVardhanReddyMax <sai.gsvr@gmail.com>
2026-09-23 18:39:38 +0530

    prediction

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 
 from src.exception import CustomException 
 # from src.components.data_ingestion import DataIngestion
@@ -42,3 +43,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
